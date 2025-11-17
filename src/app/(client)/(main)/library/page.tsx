ffc64811ec64d4612cc012e3f4f83e44744b4f44
hoapooh <hoapooh2912@gmail.com>
2025-11-17 14:49:49 +0700

    Cookies Storage uasge

--- src/app/(client)/(main)/library/page.tsx
+++ src/app/(client)/(main)/library/page.tsx
@@ -3,6 +3,7 @@ import { getQueryClient } from "@/providers/get-query-client";
 import { playlistOptions } from "@/gql/options/client-options";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import LibraryView from "@/modules/client/library/ui/views/library-view";
+import UnauthenticatedMessage from "@/modules/client/library/ui/components/unauthenticated-message";
 
 const LibraryPage = async () => {
   const queryClient = getQueryClient();
@@ -10,6 +11,16 @@ const LibraryPage = async () => {
   const cookiess = await cookies();
   const authStorage = cookiess.get("auth-storage")?.value;
   const userId = authStorage ? JSON.parse(decodeURIComponent(authStorage)).state.user?.userId : null;
+  const isAuthenticated = authStorage ? JSON.parse(decodeURIComponent(authStorage)).state.isAuthenticated : false;
+
+  if (!isAuthenticated || !userId) {
+    return (
+      <div className="w-full px-6 pt-6">
+        <h1 className="text-5xl font-bold">Library</h1>
+        <UnauthenticatedMessage />
+      </div>
+    );
+  }
 
   if (userId) {
     void queryClient.prefetchInfiniteQuery(playlistOptions(userId, undefined, 11));
